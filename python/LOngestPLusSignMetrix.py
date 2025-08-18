183113bf8a0fbab4127dc2b107aabd048db1426d
Denis Mbugua <60645043+dynamodenis@users.noreply.github.com>
2025-08-18 21:34:26 +0300

    added sqaure

--- python/LOngestPLusSignMetrix.py
+++ python/LOngestPLusSignMetrix.py
@@ -1,5 +1,16 @@
+
+
 class Solution:
-    def orderOfLargestPlusSign(self, n: int, mines: List[List[int]]) -> int:
+    """
+You are given an integer n. You have an n x n binary grid grid with all values initially 1's except for some indices given in the array mines. The ith element of the array mines is defined as mines[i] = [xi, yi] where grid[xi][yi] == 0.
+
+Return the order of the largest axis-aligned plus sign of 1's contained in grid. If there is none, return 0.
+
+An axis-aligned plus sign of 1's of order k has some center grid[r][c] == 1 along with four arms of length k - 1 going up, down, left, and right, and made of 1's. Note that there could be 0's or 1's beyond the arms of the plus sign, only the relevant area of the plus sign is checked for 1's.
+
+
+"""
+    def orderOfLargestPlusSign(self, n: int, mines: list[list[int]]) -> int:
         grid = [[1] * n for _ in range(n)]
         for (x, y) in mines:
             grid[x][y] = 0
@@ -42,11 +53,39 @@ class Solution:
 
 
 """
-You are given an integer n. You have an n x n binary grid grid with all values initially 1's except for some indices given in the array mines. The ith element of the array mines is defined as mines[i] = [xi, yi] where grid[xi][yi] == 0.
-
-Return the order of the largest axis-aligned plus sign of 1's contained in grid. If there is none, return 0.
-
-An axis-aligned plus sign of 1's of order k has some center grid[r][c] == 1 along with four arms of length k - 1 going up, down, left, and right, and made of 1's. Note that there could be 0's or 1's beyond the arms of the plus sign, only the relevant area of the plus sign is checked for 1's.
-
-
+A farmer wants to farm their land with the maximum area where good land is present. The land represented as a matrix with 1 and 0
+where 1 mean "good land" and 0s mean bad land. The farmer only wants to farm in a square of good land with the maximum area. PLease help the farmer to find the maximum
+area of the land they can farm in good land
 """
+
+
+def maximumSquare(matrix: list[list[str]])->int:
+    if not matrix or not matrix[0]:
+        return 0
+    max_sqaure = 1
+    rows = len(matrix)
+    cols = len(matrix[0])
+    dp = [[0]* (cols + 1)  for _ in range(rows + 1)]
+    
+    
+    for r in range(rows):
+        for c in range(cols):
+            
+            if matrix[r][c] == "1":
+                print(f"row {r} col {c} dp[r][c+1] {dp[r][c+1]} dp[r+1][c] {dp[r+1][c]} dp[r][c] {dp[r][c]}")
+                dp[r+1][c+1] = 1 + min(dp[r][c+1], dp[r+1][c], dp[r][c])
+                
+                max_sqaure = max(max_sqaure, dp[r+1][c+1])
+    for i in range(len(dp)):
+        print(f"{dp[i]}")
+    print(f"max_sqaure {max_sqaure}")
+    return max_sqaure
+
+matrix1 = [
+    ["1","0","1","0","0"],
+    ["1","0","1","1","1"],
+    ["1","1","1","1","1"],
+    ["1","0","0","1","0"]
+]
+
+maximumSquare(matrix1)
