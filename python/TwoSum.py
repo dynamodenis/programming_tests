ec4b81a109d05d465d32ac19024505180321250b
Denis Mbugua <60645043+dynamodenis@users.noreply.github.com>
2025-10-03 12:05:12 +0300

    added threeSum

--- python/TwoSum.py
+++ python/TwoSum.py
@@ -20,3 +20,19 @@ def twoSum(self, nums: list[int], target: int) -> list[int]:
         # According to the problem statement, there's always exactly one solution,
         # so this line should theoretically not be reached if the input is valid.
         return []
+    
+def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        left, right = 0, len(numbers)-1
+        while left <= right:
+            if numbers[left] + numbers[right] > target:
+                right -= 1
+            elif numbers[left] + numbers[right] < target:
+                left += 1
+            else:
+                return [left+1, right+1]
+            
+'''
+Input: numbers = [2,7,11,15], target = 9
+Output: [1,2]
+Explanation: The sum of 2 and 7 is 9. Therefore, index1 = 1, index2 = 2. We return [1, 2].
+'''
